a3720c744a60c71634fa9c67d5ea737c708edd9f
betaWulandari <betawulandari21@gmail.com>
2019-04-06 15:18:52 +0700

    second

---
+++ app/src/main/java/com/booksal/booksal/BottomNavigation.java
@@ -0,0 +1,13 @@
+package com.booksal.booksal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BottomNavigation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button_navigation);
+    }
+}
