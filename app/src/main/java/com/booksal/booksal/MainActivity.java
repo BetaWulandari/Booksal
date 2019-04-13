a736638e023a32ed877a13138a5dffa124cad220
betaWulandari <betawulandari21@gmail.com>
2019-04-13 15:21:20 +0700

    beta

--- app/src/main/java/com/booksal/booksal/MainActivity.java
+++ app/src/main/java/com/booksal/booksal/MainActivity.java
@@ -12,10 +12,12 @@ import android.support.v7.widget.Toolbar;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSelectedListener{
 
-    private Toolbar toolbar;
+    //This is our tablayout
     private TabLayout tabLayout;
+
+    //This is our viewPager
     private ViewPager viewPager;
 
     @Override
@@ -23,52 +25,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //Adding toolbar to the activity
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //Initializing the tablayout
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
 
-        viewPager = (ViewPager) findViewById(R.id.viewpager);
-        setupViewPager(viewPager);
+        //Adding the tabs using addTab() method
+        tabLayout.addTab(tabLayout.newTab().setText("Tab1"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab2"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab3"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
-        tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(viewPager);
+        //Initializing viewPager
+        viewPager = (ViewPager) findViewById(R.id.pager);
+
+        //Creating our pager adapter
+        Pager adapter = new Pager(getSupportFragmentManager(), tabLayout.getTabCount());
+
+        //Adding adapter to pager
+        viewPager.setAdapter(adapter);
+
+        //Adding onTabSelectedListener to swipe views
+        tabLayout.setOnTabSelectedListener(this);
     }
 
-    // cuma testing
-
-    private void setupViewPager(ViewPager viewPager) {
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new FragmentDaftar(), "DAFTAR");
-        adapter.addFragment(new FragmentMasuk(), "MASUK"); //
+    @Override
+    public void onTabSelected(TabLayout.Tab tab) {
+        viewPager.setCurrentItem(tab.getPosition());
     }
 
-    class ViewPagerAdapter extends FragmentPagerAdapter {
-        private final List<Fragment> mFragmentList = new ArrayList<>();
-        private final List<String> mFragmentTitleList = new ArrayList<>();
+    @Override
+    public void onTabUnselected(TabLayout.Tab tab) {
 
-        public ViewPagerAdapter(FragmentManager manager) {
-            super(manager);
-        }
+    }
 
-        @Override
-        public Fragment getItem(int position) {
-            return mFragmentList.get(position);
-        }
+    @Override
+    public void onTabReselected(TabLayout.Tab tab) {
 
-        @Override
-        public int getCount() {
-            return mFragmentList.size();
-        }
-
-        public void addFragment(Fragment fragment, String title) {
-            mFragmentList.add(fragment);
-            mFragmentTitleList.add(title);
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return mFragmentTitleList.get(position);
-        }
     }
 }
---
+++ app/src/main/java/com/booksal/booksal/Pager.java
@@ -0,0 +1,47 @@
+package com.booksal.booksal;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by Belal on 2/3/2016.
+ */
+//Extending FragmentStatePagerAdapter
+public class Pager extends FragmentStatePagerAdapter {
+
+    //integer to count number of tabs
+    int tabCount;
+
+    //Constructor to the class
+    public Pager(FragmentManager fm, int tabCount) {
+        super(fm);
+        //Initializing tab count
+        this.tabCount= tabCount;
+    }
+
+    //Overriding method getItem
+    @Override
+    public Fragment getItem(int position) {
+        //Returning the current tabs
+        switch (position) {
+            case 0:
+                FragmentDaftar tab1 = new FragmentDaftar();
+                return tab1;
+            case 1:
+                FragmentMasuk tab2 = new FragmentMasuk();
+                return tab2;
+            case 2:
+                boklap tab3 = new boklap()               ;
+                return tab3;
+            default:
+                return null;
+        }
+    }
+
+    //Overriden method getCount to get the number of tabs
+    @Override
+    public int getCount() {
+        return tabCount;
+    }
+}
